ec9600c9a55cf09cb1a474be2176adc743985cdd
manishbalyan <balyan05.manish@gmail.com>
2015-06-04 16:25:52 +0530

    Flask Blog App Version 1.0

---
+++ sql.py
@@ -0,0 +1,18 @@
+# create a sqlite table and populate it with data
+
+import sqlite3
+
+#create a new database if the database does not exist
+
+with sqlite3.connect("blog.db") as connection:
+	# get a cursor object used to execute sql commands
+	c = connection.cursor()
+
+	#create the table
+	c.execute(""" CREATE TABLE posts(title TEXT ,posts TEXT)""")
+
+	#insert dummy data into the table
+	c.execute('INSERT INTO posts VALUES("Good","I\'m good.")')
+	c.execute('INSERT INTO posts VALUES("Well","I\'m well.")')
+	c.execute('INSERT INTO posts VALUES("Excellent","I\'m excellent.")')
+	c.execute('INSERT INTO posts VALUES("Okay","I\'m okay.")')
